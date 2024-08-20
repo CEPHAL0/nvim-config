65e291a496cb69379307eefd16d4be9604af6802
CEPHAL0 <xaradxarma@gmail.com>
2024-08-20 10:19:48 +0545

    Added Auto pair

--- lua/user/plug_compiled.lua
+++ lua/user/plug_compiled.lua
@@ -17,9 +17,9 @@ Plug ('hrsh7th/nvim-cmp')
 Plug ('hrsh7th/cmp-nvim-lsp')
 Plug ('L3MON4D3/LuaSnip')
 Plug ('VonHeikemen/lsp-zero.nvim', {branch = 'v3.x'})
+
 -- Autocompletions
 Plug('hrsh7th/nvim-cmp')
-
 Plug('hrsh7th/cmp-buffer')
 Plug('hrsh7th/cmp-path')
 Plug('saadparwaiz1/cmp_luasnip')
@@ -32,7 +32,6 @@ Plug('rafamadriz/friendly-snippets')
 
 Plug 'williamboman/mason.nvim'
 Plug 'williamboman/mason-lspconfig.nvim'
+Plug 'jiangmiao/auto-pairs'
 
 vim.call('plug#end')
-
-
