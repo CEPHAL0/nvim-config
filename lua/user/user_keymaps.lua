7f604e289535fc18d8c7f88d9ff4022d9d693025
CEPHAL0 <xaradxarma@gmail.com>
2024-09-21 20:51:18 +0545

    Fixed Leader Capitalization

--- lua/user/user_keymaps.lua
+++ lua/user/user_keymaps.lua
@@ -3,7 +3,7 @@ local opts = {noremap = true, silent=true}
 
 keymap("n", "<Space>", "", opts)
 
-keymap({"n", "v"}, "<leader>t", '<cmd>echo "Hello World"<CR>')
+keymap({"n", "v"}, "<Leader>t", '<cmd>echo "Hello World"<CR>')
 keymap("n", "<C-p>", "<cmd>Telescope find_files<CR>")
 keymap({"n", "v"},"<C-b>", "<cmd>NvimTreeToggle<CR>")
 keymap("n", "<Leader>q", "<cmd>q<CR>")
@@ -11,10 +11,9 @@ keymap("n", "<Leader>p", "\"_dP")
 keymap({"n", "i", "v"}, "<C-s>", "<cmd>wall<CR><Esc>")
 keymap({"n", "i", "v"}, "<C-c>", "<cmd><CR><Esc>")
 keymap("n", "<C-u>", "<C-u>zz")
-keymap("n", "<C-u>", "<C-u>zz")
 keymap("n", "<C-d>", "<C-d>zz")
 keymap("n", "n", "nzz")
 keymap("n", "N", "Nzz")
-keymap("n", "<leader>y", "\"*y")
-keymap("v", "<leader>y", "\"*y")
-keymap("n", "<leader>Y", "\"+Y")
+keymap({"n", "v"}, "<Leader>y", "\"*y")
+keymap("n", "<Leader>Y", "\"+Y")
+
