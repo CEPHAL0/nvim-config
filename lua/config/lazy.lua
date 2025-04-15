73241bc646aff8d71746fc3cf9906914849bd149
= <=>
2025-04-15 14:02:29 +0545

    Added and fixed the config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -33,6 +33,7 @@ require("lazy").setup({
         { import = "lazyvim.plugins.extras.editor.harpoon2" },
         { import = "lazyvim.plugins.extras.formatting.prettier" },
         { import = "lazyvim.plugins.extras.coding.mini-surround" },
+        { import = "lazyvim.plugins.extras.coding.luasnip" },
 
         { import = "plugins" },
     },
